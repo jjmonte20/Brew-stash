02cb172928226aa2a9e5de6e0b2ea07e3824b6bb
DAGMAX83 <DAGMAX983@gmail.com>
2018-12-13 13:19:46 -0600

    test

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ var db = require("../models");
 var now = moment();
 // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
 passport.use(new LocalStrategy(
-  // Our user will sign in using an email, rather than a "username"
+  // Our user will sign in using a username, rather than a "username"
   {
     usernameField: "username"
   },
@@ -18,16 +18,16 @@ passport.use(new LocalStrategy(
         username: username
       }
     }).then(function(dbUser) {
-      // If there's no user with the given email
+      // If there's no user with the given username
       if (!dbUser) {
         return done(null, false, {
-          message: "Incorrect username."
+          message: "Incorrect Username."
         });
       }
-      // If there is a user with the given email, but the password the user gives us is incorrect
+      // If there is a user with the given username, but the password the user gives us is incorrect
       else if (!dbUser.validPassword(password)) {
         return done(null, false, {
-          message: "Incorrect password."
+          message: "Incorrect Password."
         });
       }
       // If none of the above, return the user
