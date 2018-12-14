fc9af481d42ffc9bbee8c7466431de13a597b5cc
Scott Allan <sallan306@gmail.com>
2018-12-14 11:59:52 -0600

    recommitted, rounded some edges on the UI

--- public/js/index.js
+++ public/js/index.js
@@ -93,28 +93,11 @@ var handleDeleteBtnClick = function() {
     refreshExamples();
   });
 };
-
-$('#loginButton').click(function(){
-  window.location.href='/login';
-})
-$('#createAccountButton').click(function(){
-  window.location.href='/createaccount';
-})
-$('.homeButton').click(function(){
-  window.location.href='/';
-})
-$('#homeButtonLogin').click(function(){
-  window.location.href='/';
-})
-$('#homeButtonCreate').click(function(){
-  window.location.href='/';
-})
-$('#headerLeft').click(function(){
-  window.location.href='/points';
-})
-$('#headerMiddle').click(function(){
-  window.location.href='/breweries';
-})
-$('#headerRight').click(function(){
-  window.location.href='/account';
-})
+$('#loginButton').click(function(){         window.location.href='/login'})
+$('#createAccountButton').click(function(){ window.location.href='/createaccount'})
+$('.homeButton').click(function(){          window.location.href='/'})
+$('#homeButtonLogin').click(function(){     window.location.href='/'})
+$('#homeButtonCreate').click(function(){    window.location.href='/'})
+$('#headerLeft').click(function(){          window.location.href='/points'})
+$('#headerMiddle').click(function(){        window.location.href='/breweries'})
+$('#headerRight').click(function(){         window.location.href='/account'})
