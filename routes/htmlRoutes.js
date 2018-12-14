9f8ed263d051e46a7e8336dcb787c0baf6f59b82
jjmonte20 <jjmonte20@gmail.com>
2018-12-14 11:38:41 -0600

    made small changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,11 @@
 var db = require("../models");
 
 module.exports = function(app) {
-<<<<<<< Updated upstream
-=======
 	// Load index page
 	app.get("/", function(req, res) {
-		db.Example.findAll({}).then(function(dbExamples) {
-			res.render("home", {
-				title: "Home Page!",
-				msg: "Welcome!",
-				examples: dbExamples
-			});
-		});
+    	db.brewery_table.findAll({}).then(function() {
+    		res.render("home");
+    	});
 	});
 
 	app.get("/points", function(req, res) {
@@ -61,35 +55,34 @@ module.exports = function(app) {
 			title: "Page Not Found"
 		});
 	});
->>>>>>> Stashed changes
-	// Load index page
-	app.get("/", function(req, res) {
-		db.brewery_table.findAll({}).then(function() {
-			res.render("index");
-		});
-	});
+	// // Load index page
+	// app.get("/", function(req, res) {
+	// 	db.brewery_table.findAll({}).then(function() {
+	// 		res.render("index");
+	// 	});
+	// });
 
-	// testing a login page
-	app.get("/login", function(req, res) {
-		// If the user already has an account send them to the members page
-		if (req.user) {
-			res.redirect("/members");
-		}
-		res.render("example");
-	});
+	// // testing a login page
+	// app.get("/login", function(req, res) {
+	// 	// If the user already has an account send them to the members page
+	// 	if (req.user) {
+	// 		res.redirect("/members");
+	// 	}
+	// 	res.render("example");
+	// });
 
-	// Load example page and pass in an example by id
-	// homepage for a specific
-	app.get("/example/:id", function(req, res) {
-		db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-			res.render("example", {
-				example: dbExample
-			});
-		});
-	});
+	// // Load example page and pass in an example by id
+	// // homepage for a specific
+	// app.get("/example/:id", function(req, res) {
+	// 	db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+	// 		res.render("example", {
+	// 			example: dbExample
+	// 		});
+	// 	});
+	// });
 
-	// Render 404 page for any unmatched routes
-	app.get("*", function(req, res) {
-		res.render("404");
-	});
+	// // Render 404 page for any unmatched routes
+	// app.get("*", function(req, res) {
+	// 	res.render("404");
+	// });
 };
