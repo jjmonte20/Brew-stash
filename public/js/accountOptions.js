0ed41e84d99bbaf2b20c66e7d06d9e91d9e74fff
jjmonte20 <jjmonte20@gmail.com>
2018-12-16 22:59:16 -0600

    able to post a brewery from the front end to the back end

--- public/js/accountOptions.js
+++ public/js/accountOptions.js
@@ -1,132 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function(){
     // here there will be some variables that are on the html file
-    var brewerName = $("#brewerName");
-    var brewerAddress = $("#brewerAddress");
+    var brewerName = $("#breweryName");
+    var brewerAddress = $("#street");
     // this is for the form for adding a brewery
-    var brewerySubmit = $("#addBrewery");
-    var userSelect = $("#user");
-
-    // ===========================================
-
-    // Here there will be a submit form triggering an action
+    var brewerySubmit = $("#cmsBrewery");
+    // need to ask about making it default to the userId
+    // var userSelect = $("#user");
     $(brewerySubmit).on("submit", handleFormSubmit);
-    // Gets to the part of the url that comes after the "?"
+    
+    // Gets the part of the url that comes after the "?"
     var url = window.location.search;
     var breweryId;
     var userId;
-    // set a flag whether it's updating or not, should be false at first
-    var updating = false;
-
-    // If we have this in our url, we pull out the post id from the user
-    // In '?brewery_id=1', breweryId is 1
-    if (url.indexOf("?brewery_id=") !== -1) {
-        breweryId = url.split("=")[1];
-        getBreweryData(breweryId, "post");
-    } 
-    // otherwise preselect it to be our brewer
-    else if (url.indexOf("?brewery_id=") !== -1){
+    
+    if (url.indexOf("?user_id=") !== -1) {
         userId = url.split("=")[1];
     }
-
-    // get the brewer, and their options
-    getBrewers();
-
-    // function for handling what happens when the form creates a new brewery
+    
     function handleFormSubmit(event) {
         event.preventDefault();
-        // won't submit the post of we are missing name and address
-        if (!brewerName.val().trim() || !brewerAddress.val().trim()){
-            return;
-        }
-
-        // Construct a new brewery
+        // console.log("I've been clicked");
+        // wont submit the post if there's a missing field
         var newBrewery = {
             brewer_name: brewerName.val().trim(),
             address: brewerAddress.val().trim(),
-            UserId: userSelect.val()
+            // hard coding in this variable until I can figure out what to do with it
+            UserId: 1
         };
-
-        // in the case that the user is updating their brewery
-        if (updating) {
-            newBrewery.id = breweryId;
-            updateBrewery(newBrewery);
-        }
-        else {
-            submitBrewery(newBrewery);
-        }
+        console.log(newBrewery);
+        submitBrewery(newBrewery);
     }
 
-    //  Submits a new brewery
     function submitBrewery(brewery) {
         $.post("/api/breweries", brewery, function() {
-            window.location.reload();
+            console.log("submitted");
         });
     }
-
-    function getBreweryData(id, type) { 
-        var queryUrl;
-        switch (type) {
-        case "brewery":
-            queryUrl = "/api/breweries/" + id;
-            break;
-        case "user":
-            queryUrl = "/api/users/" + id;
-            break;
-        default:
-            return;
-        }
-        $.get(queryUrl, function(data) {
-            if (data) {
-                console.log(data.UserId || data.id);
-                // If this brewery exists, prefill our forms with this data
-                brewerName.val(data.brewer_name);
-                brewerAddress.val(data.address);
-                userId = data.UserId || data.id;
-                // If we have a brewery with this id, set a flag for us to know to update it
-                // when we hit submit
-                updating = true;
-            }   
-        });
-    }
-
-    function getBrewers() {
-        $.get("/api/users", renderBrewerList);
-    }
-
-    function renderBrewerList(data) {
-        if (!data.length) {
-            window.location.href = "/users";
-        }
-        var rowsToAdd = [];
-        for (var i = 0; i < data.length; i++) {
-            rowsToAdd.push(createBrewerRow(data[i]));
-        }
-        brewerSelect.empty();
-        console.log(rowsToAdd);
-        console.log(authorSelect);
-        authorSelect.append(rowsToAdd);
-        authorSelect.val(userId);
-    }
-
-    function createBrewerRow(brewer) {
-        var listOption = $("<option>");
-        listOption.attr("value", User.id);
-        listOption.text(User.name);
-        return listOption;
-    }
-        // Function to either render a list of 
-        // given the request an update
-        function updateBrewery(brewery) {
-            $.ajax({
-                method: "PUT",
-                url: "/api/breweries",
-                data: brewery
-            }).then(function() {
-                window.location.href = "/breweries";
-            });
-        }
-    }
-
-    // function to
-// ================ always on the bottom
+// ===========================================
 });
+    
