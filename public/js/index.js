5af8319ffa68da457c97e1b46d81db29b7aadb6e
Scott Allan <sallan306@gmail.com>
2018-12-18 13:59:31 -0600

    merged finally

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,7 @@
 
 $("#loginButton").click(function(){ window.location.href="/login";});
 $("#createAccountButton").click(function(){ window.location.href="/createaccount";});
-$(".homeButton").click(function(){ window.location.href="/logout";});
-$("#homeButtonLogin").click(function(){ window.location.href="/";});
-$("#homeButtonCreate").click(function(){ window.location.href="/";});
-$("#headerLeft").click(function(){ window.location.href="/points";});
-$("#headerMiddle").click(function(){ window.location.href="/breweries";});
-$("#headerRight").click(function(){ window.location.href="/account";});
-$("#breweriesButton").click(function(){ window.location.href="/account";});
+$("#breweriesButton").click(function(){ window.location.href="/breweries";});
 
 $("#backButton").click(function(){ window.location.href="/";});
 
