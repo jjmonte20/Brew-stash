5eaa7aa16d874a60ccb2a3dab564d92bd9c0a5fc
Scott Allan <sallan306@gmail.com>
2018-12-18 13:48:26 -0600

    second merge conflict resolution

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,12 @@
 
 $("#loginButton").click(function(){ window.location.href="/login";});
 $("#createAccountButton").click(function(){ window.location.href="/createaccount";});
-<<<<<<< HEAD
 $(".homeButton").click(function(){ window.location.href="/logout";});
 $("#homeButtonLogin").click(function(){ window.location.href="/";});
 $("#homeButtonCreate").click(function(){ window.location.href="/";});
 $("#headerLeft").click(function(){ window.location.href="/points";});
 $("#headerMiddle").click(function(){ window.location.href="/breweries";});
 $("#headerRight").click(function(){ window.location.href="/account";});
-=======
 $("#breweriesButton").click(function(){ window.location.href="/account";});
 
 $("#backButton").click(function(){ window.location.href="/";});
@@ -26,4 +24,3 @@ function openNav() {
 function closeNav() {
 	document.getElementById("mySidenav").style.display = "none";
 }
->>>>>>> almostDone
